ab2c6befb5206026ec0dc1a33452cb609de4f0be
Hyoin <115976208+hyoin91@users.noreply.github.com>
2025-10-23 12:40:26 +0900

    Add files via upload

---
+++ tokens/typography.js
@@ -0,0 +1,104 @@
+// ⚡ 자동 생성된 typography 토큰 (build-tokens.cjs)
+export const typography = {
+  "heading": {
+    "lg": {
+      "fontFamily": "var(--font-family-body)",
+      "fontWeight": "var(--fontweight-bold)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-4xl)",
+      "letterSpacing": "var(--letterspacing-normal)"
+    },
+    "md": {
+      "fontFamily": "var(--font-family-body)",
+      "fontWeight": "var(--fontweight-bold)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-2xl)"
+    },
+    "sm": {
+      "fontFamily": "var(--font-family-body)",
+      "fontWeight": "var(--fontweight-bold)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-xl)"
+    }
+  },
+  "subtitle": {
+    "lg": {
+      "fontFamily": "var(--font-family-subtitle)",
+      "fontWeight": "var(--fontweight-semibold)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-2xl)"
+    },
+    "md": {
+      "fontFamily": "var(--font-family-body)",
+      "fontWeight": "var(--fontweight-semibold)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-xl)"
+    },
+    "sm": {
+      "fontFamily": "var(--font-family-body)",
+      "fontWeight": "var(--fontweight-semibold)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-base)"
+    },
+    "default": {
+      "fontFamily": "var(--font-family-body)",
+      "fontWeight": "var(--fontweight-semibold)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-lg)"
+    }
+  },
+  "body": {
+    "lg": {
+      "fontFamily": "var(--font-family-body)",
+      "fontWeight": "var(--fontweight-normal)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-lg)"
+    },
+    "md": {
+      "fontFamily": "var(--font-family-body)",
+      "fontWeight": "var(--fontweight-normal)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-base)"
+    },
+    "sm": {
+      "fontFamily": "var(--font-family-body)",
+      "fontWeight": "var(--fontweight-normal)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-sm)"
+    }
+  },
+  "caption": {
+    "sm": {
+      "fontFamily": "var(--font-family-body)",
+      "fontWeight": "var(--fontweight-normal)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-2xs)"
+    },
+    "xs": {
+      "fontFamily": "var(--font-family-body)",
+      "fontWeight": "var(--fontweight-normal)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-3xs)"
+    },
+    "xs_bold": {
+      "fontFamily": "var(--font-family-body)",
+      "fontWeight": "var(--fontweight-normal)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-3xs)"
+    },
+    "nomal": {
+      "fontFamily": "var(--font-family-body)",
+      "fontWeight": "var(--fontweight-normal)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-xs)"
+    }
+  },
+  "helper": {
+    "text": {
+      "fontFamily": "var(--font-family-body)",
+      "fontWeight": "var(--fontweight-normal)",
+      "lineHeight": "var(--lineheight-auto)",
+      "fontSize": "var(--fontsize-xs)"
+    }
+  }
+};
